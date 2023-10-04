fa7b172b6d1b6ba09d17ed149af4e2342da02b25
Andrey Gerlakh <andrey@MacBook-Air-Andrey.local>
2023-10-04 21:25:09 +0300

    test deploy

--- src/providers/SupabaseProvider.tsx
+++ src/providers/SupabaseProvider.tsx
@@ -2,17 +2,19 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { FC, useState } from 'react'
+import { useState } from 'react'
+
 import { Database } from '../../types_db'
 
-interface ISupabase {
+interface SupabaseProviderProps {
 	children: React.ReactNode
 }
 
-const SupabaseProvider: FC<ISupabase> = ({ children }) => {
-	const [supabaseClient] = useState(() => {
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+	const [supabaseClient] = useState(() =>
 		createClientComponentClient<Database>()
-	})
+	)
+
 	return (
 		<SessionContextProvider supabaseClient={supabaseClient}>
 			{children}
